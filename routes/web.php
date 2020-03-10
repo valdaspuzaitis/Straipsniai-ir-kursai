80d8c9388709b97dac055a17c5db19620a76e1ce
valdaspuzaitis <58857476+valdaspuzaitis@users.noreply.github.com>
2020-03-10 16:01:56 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', 'FrontPageController@index');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
 
 
 Route::get('/articles/create', 'ArticlesController@create')->middleware('auth');
